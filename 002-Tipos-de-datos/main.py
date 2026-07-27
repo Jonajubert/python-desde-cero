14c8bd2ffc27567b985812288bda8b94d4ea19d1
Jonajubert <124742963+Jonajubert@users.noreply.github.com>
2026-07-27 08:15:53 -0300

    Add example of data types in Python

---
+++ 002-Tipos-de-datos/main.py
@@ -0,0 +1,22 @@
+# -----------------------------
+# TIPOS DE DATOS EN PYTHON
+# -----------------------------
+
+# Número entero
+edad = 30
+
+# Número decimal
+altura = 1.78
+
+# Cadena de texto
+nombre = "Jonatan"
+
+# Valor verdadero o falso
+trabaja = True
+
+print("=== TIPOS DE DATOS ===\n")
+
+print("Nombre:", nombre)
+print("Edad:", edad)
+print("Altura:", altura)
+print("¿Trabaja?:", trabaja)
