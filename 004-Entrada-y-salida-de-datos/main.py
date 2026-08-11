1d12a1939d1f4e45cccd6f54cbf5ea593027566c
Jonajubert <124742963+Jonajubert@users.noreply.github.com>
2026-08-10 21:52:22 -0300

    Add input prompts for name and city

---
+++ 004-Entrada-y-salida-de-datos/main.py
@@ -0,0 +1,27 @@
+# ==========================================
+# PYTHON DESDE CERO
+# Capítulo 004 - Entrada y salida de datos
+# ==========================================
+
+
+# Mostramos un mensaje solicitando el nombre.
+print("Escribí tu nombre:")
+
+# input() espera que el usuario escriba un dato.
+# El dato ingresado se guarda en la variable nombre.
+nombre = input()
+
+
+# Mostramos un mensaje solicitando la ciudad.
+print("Escribí tu ciudad:")
+
+# Guardamos el nuevo dato en otra variable.
+ciudad = input()
+
+
+# Utilizamos las variables para mostrar el resultado.
+print()
+print("=== DATOS INGRESADOS ===")
+
+print(f"Hola, {nombre}!")
+print(f"Vivís en {ciudad}.")
