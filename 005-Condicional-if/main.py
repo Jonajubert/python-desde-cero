96938e71221f87e09396388f7d9e47648ee85ac0
Jonajubert <124742963+Jonajubert@users.noreply.github.com>
2026-08-18 08:44:36 -0300

    Add basic if condition example in main.py

---
+++ 005-Condicional-if/main.py
@@ -0,0 +1,19 @@
+# ==========================================
+# PYTHON DESDE CERO
+# Capítulo 005 - Condicional if
+# ==========================================
+
+
+# Solicitamos el nombre.
+print("Escribí tu nombre:")
+nombre = input()
+
+
+# Solicitamos la edad.
+print("Escribí tu edad:")
+edad = int(input())
+
+
+# Evaluamos la condición.
+if edad >= 18:
+    print(f"Hola {nombre}, sos mayor de edad.")
