f83ec41b155667193d7adf3728f4a8c727a9614d
Jonajubert <124742963+Jonajubert@users.noreply.github.com>
2026-09-08 08:23:35 -0300

    Add examples of for loops in Python

---
+++ 008-Bucle-for/main.py
@@ -0,0 +1,27 @@
+# ==========================================
+# PYTHON DESDE CERO
+# Capítulo 008 - Bucle for
+# ==========================================
+
+
+# EJEMPLO 1
+# Recorremos números del 1 al 5.
+
+for numero in range(1, 6):
+    print(numero)
+
+
+# EJEMPLO 2
+# Recorremos los elementos de una lista.
+
+lenguajes = ["Python", "C#", "JavaScript"]
+
+for lenguaje in lenguajes:
+    print(lenguaje)
+
+
+# EJEMPLO 3
+# Utilizamos un incremento diferente.
+
+for numero in range(0, 11, 2):
+    print(numero)
